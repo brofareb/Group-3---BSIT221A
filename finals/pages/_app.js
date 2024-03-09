8d7672d4e38f86ee84fb25a2f746bfd3cfe741d3
brofareb <euwaynebrofar@gmail.com>
2024-03-09 23:40:05 +0800

    contact-us 1st

--- finals/pages/_app.js
+++ finals/pages/_app.js
@@ -2,21 +2,16 @@ import "/styles/footer.scss"
 import "/styles/header.scss"
 import "/styles/home.scss"
 import "/styles/about.scss"
-
+import "/styles/contactus.scss"
 
 import Header from "/components/constants/Header"
 import Footer from "/components/constants/Footer"
-
-
-
 
 
 function App({ Component, pageProps }) {
   if (Component.getLayout) {
     return Component.getLayout(<Component {...pageProps} />)
   }
-  
-  
   
   return (
   <>
