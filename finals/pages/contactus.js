7032eff112d8b1982be18f57f007c985c22444d6
brofareb <euwaynebrofar@gmail.com>
2024-03-10 02:56:26 +0800

    updated contact_us + scss
    (kulang pa sa scss)

--- finals/pages/contactus.js
+++ finals/pages/contactus.js
@@ -2,18 +2,15 @@
 export default function ContactUS(){
     return(
         <>
-            
-
-                <div className ="contact-title">
-                    <h1>
-                        CONTACT US
-                    </h1>
-                </div>
+            <div className ="contact-title">
+                <h1>
+                    CONTACT US
+                </h1>
+            </div>
+            <div className = "body">
                 <div className = "contact">
-                <p>
-                        Have a question, suggestion, or just want to share your culinary triumphs with us? We'd love to hear from you! 
-                         <br />Contact The Recipe Room through the following channels:
-                    </p>
+                    <p>Have a question, suggestion, or just want to share your culinary triumphs with us? We'd love to hear from you! 
+                        Contact The Recipe Room through the following channels:</p>
                     <ul className = "contact-list">
                         <li>Email: support@reciperoom.com</li>
                         <li>Phone: 0912-345-6789</li>
@@ -24,11 +21,11 @@ export default function ContactUS(){
                         General Inquiries:
                         For general inquiries or feedback, drop us a line!
                     </p>
-                    <ul className = "info-email">
-                        <li>Email: info@reciperoom.com</li>
-                    </ul>
+                        <ul className = "info-email">
+                            <li>Email: info@reciperoom.com</li>
+                        </ul>
                 </div>
-
+            </div>
             
         </>
     )
